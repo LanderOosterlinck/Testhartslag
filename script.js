d96e72b05ee745e12aa61d2ab8561a00b0edb249
LanderOosterlinck <43674185+LanderOosterlinck@users.noreply.github.com>
2020-01-21 10:39:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 class HeartRateMonitor {
-  constructor() {
+  constructor(name, characterid) {
+    //Service ID for Heart Rate class devices
+    //https://www.bluetooth.com/specifications/gatt/services/
     this.SERVICE_ID = 0x180D;
+    //Char id for Heart Rate Measurement
+    //https://www.bluetooth.com/specifications/gatt/characteristics/
     this.CHARACTERISTIC_ID = 0x2A37;
-    
+ 
     this.hrElement_ = document.getElementById('hr');
     this.avgElement_ = document.getElementById('avg');
-    
-    this.resetAverage_();
+    this.name = name;
+    this.characterid = characterid;
+ 
+    this.score_ = 0;
+    this.MHR_ = 140;
+    this.hasProperReading_ = false;
+ 
+    this.gameid_ = 0;
+    this.LowestHR_ = 0;
+    this.HighestHR_ = 0;
+ 
+    this.CdcHighest_ = 0;
+    this.CdcAim_ = 0;
+    this.playerStillPlaying_ = true;
+    this.resetEverything_();
   }
-
+ 
   computeAverage_() {
     if (this.timeSum_ > 0) {
       let avg = this.hrSum_ / ((this.timeSum_) * 2);
@@ -16,14 +33,31 @@ class HeartRateMonitor {
     }
     return '0.0';
   }
-
-  resetAverage_() {
+ 
+  resetEverything_() {
     this.lastTick_ = 0;
     this.lastHr_ = 0;
     this.hrSum_ = 0;
     this.timeSum_ = 0;
+ 
+    this.RHR_ = 0;
+    this.HRR_ = 0;
+ 
+    this.hasProperReading_ = false;
   }
-  
+ 
+  setCdcValues_() {
+    this.CdcHighest_ = this.lastHr_;
+    this.CdcAim_ = this.CdcHighest_ - 20 ;
+    console.log(this.CdcAim_);
+}
+ 
+ 
+  setHRR_() {
+    this.RHR_ = this.lastHr_;
+    this.HRR_ = (140 - this.RHR_);
+  }
+ 
   parseHeartRate_(data) {
     let flags = data.getUint8(0);
     if (flags & 0x1) {
@@ -31,13 +65,12 @@ class HeartRateMonitor {
     }
     return data.getUint8(1);
   }
-
+ 
   onHeartRateChanged_(event) {
-    console.log(event);
-    let dataView =  event.target.value;
+    let dataView = event.target.value;
     let tick = (new Date()).getTime();
     let hr = this.parseHeartRate_(dataView);
-    
+ 
     // Ignore readings where the HR or last HR value is 0 - treat this as a
     // failed reading from the sensor.
     if (this.lastTick_ && hr && this.lastHr_) {
@@ -46,36 +79,45 @@ class HeartRateMonitor {
     }
     this.lastTick_ = tick;
     this.lastHr_ = hr;
-    
-    this.hrElement_.textContent = hr;
-    this.avgElement_.textContent = this.computeAverage_();
+    this.HRPCT_ = Math.floor((hr - this.RHR_) / (this.HRR_) * 100);
+ 
+    if (this.hasProperReading_ == false && hr != 0) {
+      validUser();
+      this.hasProperReading_ = true;
+    }
+ 
+ 
   }
-
+ 
   handleCharacteristic_(characteristic) {
     characteristic.addEventListener('characteristicvaluechanged',
-        event => this.onHeartRateChanged_(event));
+      event => this.onHeartRateChanged_(event));
     return characteristic.startNotifications();
   }
-
+ 
   start() {
-    this.resetAverage_();
-    let options = {filters: [{
-      services: [this.SERVICE_ID],
-      namePrefix: 'Polar H7'
-    }]};
+    this.resetEverything_();
+    let options = {
+      filters: [{
+        //Only show devices that are classified as Heart Rate Sensors
+        services: [this.SERVICE_ID]
+      }]
+    };
     navigator.bluetooth.requestDevice(options)
-        .then(device => {
-          return device.gatt.connect();
-        })
-        .then(server => {
-          return server.getPrimaryService(this.SERVICE_ID);
-        })
-        .then(service => {
-          return service.getCharacteristic(this.CHARACTERISTIC_ID);
-        })
-        .then(characteristic => this.handleCharacteristic_(characteristic))
-        .catch(error => {
-          console.log('Error: ' + error);
-        });
+      .then(device => {
+        return device.gatt.connect();
+      })
+      .then(server => {
+        return server.getPrimaryService(this.SERVICE_ID);
+      })
+      .then(service => {
+        return service.getCharacteristic(this.CHARACTERISTIC_ID);
+      })
+      .then(characteristic => this.handleCharacteristic_(characteristic))
+      .catch(error => {
+        console.log('Error: ' + error);
+        deleteInvalidUser();
+      });
   }
+ 
 }
